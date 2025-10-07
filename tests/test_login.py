c9940fcd131190f93dff6a9ffed9ff79c55777e8
Bogdan Petrov <awgedepartment@gmail.com>
2025-10-07 14:10:06 +0700

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,21 +3,15 @@ from playwright.sync_api import sync_playwright
 def test_example():
     assert True
 
-
 def test_login_saucedemo():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto("https://www.saucedemo.com/")
-
-        # Вводим логин и пароль
         page.fill("#user-name", "standard_user")
         page.fill("#password", "secret_sauce")
-
-        # Кликаем по кнопке входа
         page.click("#login-button")
-
-        # Проверяем, что произошёл переход на страницу каталога
         assert "inventory" in page.url
-
         browser.close()
+
+
